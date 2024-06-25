cf5901ec73111f4b6af0f78ea083a02bb5788182
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-06-25 19:29:59 +0200

    removed unused stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { CreatePost } from "~/app/_components/create-post";
+
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
@@ -52,24 +52,5 @@ export default async function Home() {
       </section>
       <Footer />
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
-
-  const latestPost = await api.post.getLatest();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
